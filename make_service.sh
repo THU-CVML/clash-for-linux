6b56ef1e119ae7b289ed84ab8ff093f46874f9aa
叶璨铭 (Ye Canming) <ycm24@mails.tsinghua.edu.cn>
2024-11-07 16:57:05 +0800

    fix: bash warnings

--- make_service.sh
+++ make_service.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 sudo cp clash_for_linux_ycm.service /etc/systemd/system/
 sudo systemctl enable clash_for_linux_ycm.service
--- start.sh
+++ start.sh
@@ -16,8 +16,6 @@ chmod +x $Server_Dir/bin/*
 chmod +x $Server_Dir/scripts/*
 chmod +x $Server_Dir/tools/subconverter/subconverter
 
-
-
 #################### 变量设置 ####################
 
 Conf_Dir="$Server_Dir/conf"
@@ -29,8 +27,6 @@ URL=${CLASH_URL:?Error: CLASH_URL variable is not set or empty}
 
 # 获取 CLASH_SECRET 值，如果不存在则生成一个随机数
 Secret=${CLASH_SECRET:-$(openssl rand -hex 32)}
-
-
 
 #################### 函数定义 ####################
 
@@ -70,8 +66,6 @@ if_success() {
 	fi
 }
 
-
-
 #################### 任务执行 ####################
 
 ## 获取CPU架构信息
@@ -84,7 +78,6 @@ if [[ -z "$CpuArch" ]]; then
 	exit 1
 fi
 
-
 ## 临时取消环境变量
 unset http_proxy
 unset https_proxy
@@ -92,7 +85,6 @@ unset no_proxy
 unset HTTP_PROXY
 unset HTTPS_PROXY
 unset NO_PROXY
-
 
 ## Clash 订阅地址检测及配置文件下载
 # 检查url是否有效
@@ -114,8 +106,7 @@ curl -L -k -sS --retry 5 -m 10 -o $Temp_Dir/clash.yaml $URL
 ReturnStatus=$?
 if [ $ReturnStatus -ne 0 ]; then
 	# 如果使用curl下载失败，尝试使用wget进行下载
-	for i in {1..10}
-	do
+	for i in {1..10}; do
 		wget -q --no-check-certificate -O $Temp_Dir/clash.yaml $URL
 		ReturnStatus=$?
 		if [ $ReturnStatus -eq 0 ]; then
@@ -130,46 +121,46 @@ if_success $Text3 $Text4 $ReturnStatus
 # 重命名clash配置文件
 \cp -a $Temp_Dir/clash.yaml $Temp_Dir/clash_config.yaml
 
-
 ## 判断订阅内容是否符合clash配置文件标准，尝试转换（当前不支持对 x86_64 以外的CPU架构服务器进行clash配置文件检测和转换，此功能将在后续添加）
-if [[ $CpuArch =~ "x86_64" || $CpuArch =~ "amd64"  ]]; then
+if [[ $CpuArch =~ "x86_64" || $CpuArch =~ "amd64" ]]; then
 	echo -e '\n判断订阅内容是否符合clash配置文件标准:'
 	bash $Server_Dir/scripts/clash_profile_conversion.sh
 	sleep 3
 fi
 
-
 ## Clash 配置文件重新格式化及配置
-# 取出代理相关配置 
+# 取出代理相关配置
 #sed -n '/^proxies:/,$p' $Temp_Dir/clash.yaml > $Temp_Dir/proxy.txt
-sed -n '/^proxies:/,$p' $Temp_Dir/clash_config.yaml > $Temp_Dir/proxy.txt
+sed -n '/^proxies:/,$p' $Temp_Dir/clash_config.yaml >$Temp_Dir/proxy.txt
 
 # 合并形成新的config.yaml
-cat $Temp_Dir/templete_config.yaml > $Temp_Dir/config.yaml
-cat $Temp_Dir/proxy.txt >> $Temp_Dir/config.yaml
+cat $Temp_Dir/templete_config.yaml >$Temp_Dir/config.yaml
+cat $Temp_Dir/proxy.txt >>$Temp_Dir/config.yaml
 \cp $Temp_Dir/config.yaml $Conf_Dir/
 
 # Configure Clash Dashboard
-Work_Dir=$(cd $(dirname $0); pwd)
+Work_Dir=$(
+	cd $(dirname $0)
+	pwd
+)
 Dashboard_Dir="${Work_Dir}/dashboard/public"
 sed -ri "s@^# external-ui:.*@external-ui: ${Dashboard_Dir}@g" $Conf_Dir/config.yaml
 sed -r -i '/^secret: /s@(secret: ).*@\1'${Secret}'@g' $Conf_Dir/config.yaml
-
 
 ## 启动Clash服务
 echo -e '\n正在启动Clash服务...'
 Text5="服务启动成功！"
 Text6="服务启动失败！"
-if [[ $CpuArch =~ "x86_64" || $CpuArch =~ "amd64"  ]]; then
-	nohup $Server_Dir/bin/clash-linux-amd64 -d $Conf_Dir &> $Log_Dir/clash.log &
+if [[ $CpuArch =~ "x86_64" || $CpuArch =~ "amd64" ]]; then
+	nohup $Server_Dir/bin/clash-linux-amd64 -d $Conf_Dir &>$Log_Dir/clash.log &
 	ReturnStatus=$?
 	if_success $Text5 $Text6 $ReturnStatus
-elif [[ $CpuArch =~ "aarch64" ||  $CpuArch =~ "arm64" ]]; then
-	nohup $Server_Dir/bin/clash-linux-arm64 -d $Conf_Dir &> $Log_Dir/clash.log &
+elif [[ $CpuArch =~ "aarch64" || $CpuArch =~ "arm64" ]]; then
+	nohup $Server_Dir/bin/clash-linux-arm64 -d $Conf_Dir &>$Log_Dir/clash.log &
 	ReturnStatus=$?
 	if_success $Text5 $Text6 $ReturnStatus
 elif [[ $CpuArch =~ "armv7" ]]; then
-	nohup $Server_Dir/bin/clash-linux-armv7 -d $Conf_Dir &> $Log_Dir/clash.log &
+	nohup $Server_Dir/bin/clash-linux-armv7 -d $Conf_Dir &>$Log_Dir/clash.log &
 	ReturnStatus=$?
 	if_success $Text5 $Text6 $ReturnStatus
 else
@@ -184,7 +175,7 @@ echo -e "Secret: ${Secret}"
 echo ''
 
 # 添加环境变量(root权限)
-cat>/etc/profile.d/clash.sh<<EOF
+cat >/etc/profile.d/clash.sh <<EOF
 # 开启系统代理
 function proxy_on() {
 	export http_proxy=http://127.0.0.1:7890
